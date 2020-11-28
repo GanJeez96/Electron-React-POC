05913e72d7bbc95d9bfac7597cf2844fea332657
GanJeez96 <46042452+GanJeez96@users.noreply.github.com>
2020-11-28 21:51:02 +0530

    Tested & Refactored CI pipelines (#3)
    * Testing push events in CI pipeline
    
    * CI refactor
    
    * yml fix
    
    * Removed old yml files
    
    * Added multiple OS
    
    * Added multiple OS based jobs
    
    * removed build from yml

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testing CI pipeline for push events
         </p>
         <a
           className="App-link"
